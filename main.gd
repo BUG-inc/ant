4065584ea3e93b7941ff253e79ea7b363346c732
Diogo Almeida <digalmeda@gmail.com>
2021-11-13 21:14:49 +0000

    Update queen

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var _swap_test_level = load("res://levels/test_levels/switch_camera_test.tscn")
+var _swap_test_level = load("res://levels/test_levels/queen_test.tscn")
 
 func _on_Button_pressed() -> void:
 	$menu.queue_free()
