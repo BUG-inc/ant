0d355edf2f576e9ab2cc1032c50bb84b220fce82
Joshua Haustein <haustein@kth.se>
2021-11-14 12:31:43 +0000

    feat: ant directional bias

--- entities/npc_ant.gd
+++ entities/npc_ant.gd
@@ -6,6 +6,9 @@ var _gen = RandomNumberGenerator.new()
 var _time_since_change := 0.0
 var pheromone_map: PheromoneMap = null
 export (int) var pheromone_range = 2
+export (float) var pheromone_cone_size = 180.0 / 180.0 * PI  # total angle of the cone in radians
+var sensed_pheromones: Array = []
+export (bool) var enable_debug_drawing = false
 
 func set_pheromones_map(map: PheromoneMap):
 	pheromone_map = map
@@ -24,6 +27,8 @@ func _physics_process(delta: float) -> void:
 		_dir = _change_dir()
 
 	velocity = move_and_slide(speed * _dir)
+	if enable_debug_drawing:
+		update()
 	
 func _sum(values: Array) -> float:
 	var acc: float = 0.0
@@ -31,10 +36,16 @@ func _sum(values: Array) -> float:
 		acc += v
 	return acc
 
+func _max(values: Array) -> float:
+	var max_val: float = -INF
+	for v in values:
+		max_val = max(v, max_val)
+	return max_val
+
 func _weighted_sampling(values: Array) -> int:
 	"""
 	Randomly sample an index of the given array, where the probability of an
-	index i being sampled is ~values[i].
+	index i being sampled is p(i)~values[i].
 	"""
 	var p = randf()
 	var normalizer = _sum(values)
@@ -47,16 +58,36 @@ func _weighted_sampling(values: Array) -> int:
 			return i
 	# this shouldn't happen
 	return len(values) - 1
+	
+func _sample_random_dir() -> Vector2:
+	return Vector2(1.0, 0.0).rotated(_gen.randf_range(0.0, 2.0 * PI)) 
 
-func _change_dir() -> float:
+func _change_dir() -> Vector2:
 	_gen.randomize()
-	var dir = Vector2(0, 0)
 	if pheromone_map != null:
+		sensed_pheromones = []
 		var pheromones = pheromone_map.get_pheromone_levels(global_position, pheromone_range)
-		if len(pheromones) > 0:
-			var index = _weighted_sampling(pheromones[0])
-			dir = pheromones[1][index] - global_position
-	if dir.length() == 0.0:
-		dir = Vector2(_gen.randf_range(-1, 1), _gen.randf_range(-1, 1))  # bias towards goal
-	dir = dir.normalized()
-	return dir
+		match pheromones:
+			[]:
+				return _sample_random_dir()
+			[var values, var positions]:
+				# find pheromones in cone
+				var dirs: Array = []
+				var in_cone_values = []
+				for i in len(values):
+					var dir : Vector2 = (positions[i] - global_position).normalized()
+					var angle: float = abs(_dir.angle_to(dir))
+					if angle <= pheromone_cone_size / 2.0:
+						sensed_pheromones.append(positions[i])
+						dirs.append(dir)
+						in_cone_values.append(values[i])
+				# in case we don't have any values in cone, sample a random direction
+				if len(in_cone_values) == 0 or _max(in_cone_values) == 0.0:
+					return _sample_random_dir()
+				return dirs[_weighted_sampling(in_cone_values)]
+	return _sample_random_dir()
+
+func _draw():
+	if enable_debug_drawing:
+		for point in sensed_pheromones:
+			draw_circle(to_local(point), 2.1, Color(0,1,0,1))
